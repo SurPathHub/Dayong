40b8d90fb9440ea4cd49b5e04283526cbadb214b
Hju Kneyck Flores <hjucode@gmail.com>
2021-08-23 21:18:43 +0800

    feat: enable the use of environment variables

--- dayong/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Dayong's base directory.
-"""
--- dayong/bot.py
+++ dayong/bot.py
@@ -15,22 +15,33 @@ from discord.ext.commands.errors import (
     ExtensionNotFound,
     NoEntryPointError,
 )
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent
+ROOT_DIR = BASE_DIR.parent
+
+# Parse the .env file and load the environment variables.
+load_dotenv()
+
+# Load any environment variables or secrets necessary for Dayong to run.
+BOT_PREFIX = os.getenv("BOT_PREFIX")
+TOKEN = os.getenv("YOUR_BOT_TOKEN_HERE")
+APPLICATION_ID = os.getenv("APPLICATION_ID")
+OWNERS = os.getenv("OWNERS").split(",")
 
 
 def load_config_file() -> Any:
-    """Parse the configuration file.
+    """Parse the `config.json` file from the project root directory.
 
     Returns:
         Any: The key-value pair contained in the file.
     """
     config_file = "config.json"
-    if not os.path.isfile(os.path.join(BASE_DIR, config_file)):
+    if not os.path.isfile(os.path.join(ROOT_DIR, config_file)):
         sys.exit(f"'{config_file}' cannot be found!")
 
-    with open("config.json", "r", encoding="utf-8") as confile:
-        return json.load(confile)
+    with open(config_file, "r", encoding="utf-8") as cfp:
+        return json.load(cfp)
 
 
 def load_extensions() -> list[str]:
@@ -41,13 +52,16 @@ def load_extensions() -> list[str]:
             omitted.
     """
     extensions: list[str] = []
-    for file in os.listdir("cogs"):
-        if file.endswith(".py"):
+
+    for file in os.listdir(os.path.join(BASE_DIR, "cogs")):
+        # Append cog modules and ignore dunder files.
+        if file.endswith(".py") and "__" not in file:
             extensions.append(file.replace(".py", ""))
+
     return extensions
 
 
-def setup_bot() -> None:
+def setup_bot(use_config: bool = False) -> None:
     """Run Dayong with the configuration specified in the `config.json` file.
 
     The keys in the config file are as listed here:
@@ -59,10 +73,22 @@ def setup_bot() -> None:
     This bot uses default intents (events restrictions). For more information
     on intents, please refer to discord.py's documentation:
     - https://discordpy.readthedocs.io/en/latest/intents.html
+
+    Args:
+        use_config (bool, optional): If True, use the `config.json` file from
+            the project root directory. Defaults to using environment
+            variables.
     """
-    conf = load_config_file()
+    if use_config is True:
+        conf = load_config_file()
+        conf_prefix = conf["bot_prefix"]
+        conf_token = conf["token"]
+    else:
+        conf_prefix = BOT_PREFIX
+        conf_token = TOKEN
+
     exts = load_extensions()
-    bot = Bot(conf["bot_prefix"], intents=Intents.default())
+    bot = Bot(conf_prefix, intents=Intents.default())
 
     for ext in exts:
         try:
@@ -77,8 +103,7 @@ def setup_bot() -> None:
         except ExtensionFailed:
             pass
 
-    # Run the bot with the token
-    bot.run(conf["token"])
+    bot.run(conf_token)
 
 
 if __name__ == "__main__":
