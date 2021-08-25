b8cff324367565d972f336aac43a03104cb9c7c3
Hju Kneyck Flores <hjucode@gmail.com>
2021-08-25 09:39:29 +0800

    refactor: move `ext_exception_handler()` to `dayong.cogs.utils`

--- dayong/exceptions.py
+++ dayong/exceptions.py
@@ -4,14 +4,7 @@ of Dayong and the exception handlers that are designed to handle some of them.
 """
 from functools import wraps
 
-from discord.ext.commands.errors import (  # type: ignore
-    DiscordException,
-    ExtensionAlreadyLoaded,
-    ExtensionFailed,
-    ExtensionNotFound,
-    ExtensionNotLoaded,
-    NoEntryPointError,
-)
+from discord.ext.commands.errors import DiscordException  # type: ignore
 
 
 def exception_handler(func):
@@ -25,42 +18,5 @@ def exception_handler(func):
             raise DiscordException(clt_err) from clt_err
         except Exception as gen_err:
             raise Exception(gen_err) from gen_err
-
-    return wrapper
-
-
-def ext_exception_handler(func):
-    """Handle any raised `discord.ext.commands.errors.ExtensionError`
-    subclass.
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        async def tmp():
-            failed_msg = "Failed to load {ext}. "
-            try:
-                await func(*args, **kwargs)
-            except ExtensionNotFound as enf:
-                return (
-                    "Unable to find {ext}",
-                    f"```python\n{enf}```",
-                )
-
-            except NoEntryPointError as npe:
-                return (
-                    failed_msg + "Check its `setup` entry point.",
-                    f"```python\n{npe}```",
-                )
-            except ExtensionFailed as efe:
-                return (
-                    failed_msg + "Check its module or `setup` entry point.",
-                    f"```python\n{efe}```",
-                )
-            except ExtensionAlreadyLoaded:
-                pass
-            except ExtensionNotLoaded:
-                pass
-
-        return tmp()
 
     return wrapper
