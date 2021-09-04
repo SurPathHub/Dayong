4c29ecb4e2c65fe6ef07ab4e91076c4903a22a29
Hju Kneyck Flores <hjucode@gmail.com>
2021-09-04 22:49:25 +0800

    feat: allow db connections

---
+++ dayong/db.py
@@ -0,0 +1,56 @@
+"""
+dayong.db
+~~~~~~~~~
+
+Database models, objects, and connections used within Dayong.
+"""
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel import Field, SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+
+class TableModel(SQLModel):  # pylint: disable=R0903
+    """Base class for database models."""
+
+
+class AnonymousMessageTB(TableModel, table=True):
+    """Table model for anonymous messages."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: str
+    username: str
+    nickname: str
+    message: str
+
+
+class Database(ABC):
+    """Common interface for Database objects."""
+
+    @abstractmethod
+    async def create_table(self) -> None:
+        """Create database table."""
+
+    @abstractmethod
+    async def add_row(self, model: TableModel) -> None:
+        """Add row to type of `db.TableModel`."""
+
+
+class PostgreSQLDatabase(Database):
+    """Class for handling PostgresSQL database connection."""
+
+    def __init__(self, connection_uri: str):
+        self.engine = create_async_engine(connection_uri)
+
+    async def create_table(self) -> None:
+        async with self.engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
+
+    async def add_row(self, model: TableModel) -> None:
+        async with AsyncSession(self.engine) as session:
+            session.add(model)
+            await session.commit()
+
+    # To access data, see https://github.com/tiangolo/sqlmodel/pull/58
