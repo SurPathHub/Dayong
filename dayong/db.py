560f45c1245641a9035c88f0f8bbf84581f77956
Hju Kneyck Flores <hjucode@gmail.com>
2021-09-05 21:51:43 +0800

    feat!: moderate anon messages on sql database

--- dayong/db.py
+++ dayong/db.py
@@ -2,55 +2,45 @@
 dayong.db
 ~~~~~~~~~
 
-Database models, objects, and connections used within Dayong.
+Database connections used within Dayong.
 """
-from abc import ABC, abstractmethod
-from typing import Optional
-
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import Field, SQLModel
+from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-
-class TableModel(SQLModel):  # pylint: disable=R0903
-    """Base class for database models."""
+from dayong.models import TableModel
 
 
-class AnonymousMessageTB(TableModel, table=True):
-    """Table model for anonymous messages."""
-
-    id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: str
-    username: str
-    nickname: str
-    message: str
-
-
-class Database(ABC):
-    """Common interface for Database objects."""
-
-    @abstractmethod
-    async def create_table(self) -> None:
-        """Create database table."""
-
-    @abstractmethod
-    async def add_row(self, model: TableModel) -> None:
-        """Add row to type of `db.TableModel`."""
-
-
-class SQLDatabase(Database):
-    """Class for handling SQL database connections."""
+class DBConnection:
+    """Provides methods for interacting with the table models."""
 
     def __init__(self, connection_uri: str):
         self.engine = create_async_engine(connection_uri)
 
     async def create_table(self) -> None:
+        """Create database table."""
         async with self.engine.begin() as conn:
             await conn.run_sync(SQLModel.metadata.create_all)
 
     async def add_row(self, model: TableModel) -> None:
+        """Add row to type of `db.TableModel`."""
         async with AsyncSession(self.engine) as session:
             session.add(model)
             await session.commit()
 
-    # To access data, see https://github.com/tiangolo/sqlmodel/pull/58
+    # NOTE: Wait 'til https://github.com/tiangolo/sqlmodel/pull/58 gets merged
+    # async def get_row_by_username(self, username: str) -> Union[_T, None]:
+    #     """Fetch information on author by username.
+    #     Args:
+    #         username (str): The server member's username.
+    #     Returns:
+    #         Union[_T, None]: Scalar value if the username exists, None
+    #             otherwise.
+    #     """
+    #
+    # async with AsyncSession(AnonymousMessageTable) as session:
+    #     statement = select(self.anon_table).where(
+    #         AnonymousMessageTable.username == username
+    #     )
+    #     result = await session.exec(statement)
+    #     return result.first()
