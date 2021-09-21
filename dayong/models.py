a7ec20404813167c9e7bb37e7905caff65518de2
Hju Kneyck Flores <hjucode@gmail.com>
2021-09-21 16:10:16 +0800

    chore: rewrite codebase (#4)
    - Switch bot framework from discord.py to hikari
    - Use pydantic for data type validation
    - Use rich for prettier console logging
    - Add settings module for storing constants used accross Dayong's modules
    - Rename extensions directory from cogs to components
    - Separate bot config from bot session
    - Implement interfaces
    
    NOTE: For interfaces, see PEP 544 -- Protocols: Structural subtyping (static duck typing)

--- dayong/models.py
+++ dayong/models.py
@@ -2,33 +2,14 @@
 dayong.models
 ~~~~~~~~~~~~~
 
-<<<<<<< HEAD
-A model maps to a single database table. It contains fields and behaviors of the data
-stored in the database.
-=======
 A model maps to a single database table. It contains fields and behaviors of
 the data stored in the database.
->>>>>>> 89e51bc... feat: moderate anon messages on sql database
 """
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
 
 
-<<<<<<< HEAD
-class Message(SQLModel):
-    """Base model class for message table models."""
-
-    message_id: str
-
-
-class AnonMessage(Message, table=True):
-    """Table model for anonymized guild messages."""
-
-    # pyright cannot recognize the type of SQLModel.__tablename__
-    # See: https://github.com/tiangolo/sqlmodel/issues/98
-    __tablename__ = "anon_messages"  # type: ignore
-=======
 class TableModel(SQLModel):  # pylint: disable=R0903
     """Base class for table models.
 
@@ -41,7 +22,6 @@ class TableModel(SQLModel):  # pylint: disable=R0903
 class AnonymousMessageTable(TableModel, table=True):
     """Table model for anonymous messages."""
 
->>>>>>> 89e51bc... feat: moderate anon messages on sql database
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: str
     username: str
--- docs/README.md
+++
@@ -1,6 +0,0 @@
-# Table of contents
-
-* [Introduction](../README.md)
-* [Installation](./installation.md)
-* [Setup](./setup.md)
-* [Commands](./commands.md)
