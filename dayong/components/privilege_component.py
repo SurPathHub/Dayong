532ca9ff69ebc566e5755c46258ab54415d72561
Hju Kneyck Flores <hjucode@gmail.com>
2021-09-22 08:16:01 +0800

    chore: follow up rewrite
    - Add Dayong's custom banner
    - Add standard ping command
    - Properly implement components
    - Fix flake8 installation to include plugins
    - Include guild ID in configuration
    - Move uvloop setup to settings.py
    - Rename protocols module to interfaces
    - Apply formatters and linters

---
+++ dayong/components/privilege_component.py
@@ -0,0 +1,37 @@
+"""
+dayong.components.privilege
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Commands and utilites for guild members with permissions.
+"""
+import time
+
+import tanjun
+
+component = tanjun.Component()
+
+
+@component.with_command
+@tanjun.with_author_permission_check(32)
+@tanjun.as_message_command("ping")
+async def ping(ctx: tanjun.abc.Context, /) -> None:
+    """Respond with pong and the time it takes for it to reach the Discord using
+    WebSocket and REST API endpoints.
+
+    Args:
+        ctx (Context): Instantiated subclass of `tanjun.abc.Context`.
+    """
+    start_time = time.perf_counter()
+    await ctx.respond(content="( ͡° ͜ʖ ͡°)")
+    time_taken = (time.perf_counter() - start_time) * 1_000
+    heartbeat_latency = (
+        ctx.shards.heartbeat_latency * 1_000 if ctx.shards else float("NAN")
+    )
+    await ctx.edit_last_response(
+        f"PONG\n - REST: {time_taken:.0f}ms\n - Gateway: {heartbeat_latency:.0f}ms"
+    )
+
+
+@tanjun.as_loader
+def load_examples(client: tanjun.Client) -> None:
+    client.add_component(component.copy())
