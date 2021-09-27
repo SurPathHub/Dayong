74d7025f607ee2f202119051605743b9af65a3c8
Hju Kneyck Flores <hjucode@gmail.com>
2021-09-28 02:04:03 +0800

    docs: update docstrings

--- dayong/components/slash_component.py
+++ dayong/components/slash_component.py
@@ -122,40 +122,48 @@ async def anon_command(
     Args:
         ctx (tanjun.abc.SlashContext): Interface of a context.
         message (str): The message to anonymize.
-        database (MessageDBProto): Interface for database message tables. This is a
+        database (MessageDBProto): Interface for a database message table. This is a
             registered type dependency and is injected by the client.
         config (DayongConfig): An instance of `dayong.configs.DayongConfig`. Also a
             registered type dependency and is injected by the client.
     """
     await ctx.defer()
-    if isinstance(ctx.member, hikari.InteractionMember) and isinstance(
-        channel := await ctx.fetch_channel(), hikari.TextableChannel
-    ):
-        await database.create_table()
-        message_id = await randomize_id(ctx.member.username)
-        await database.add_row(
-            AnonMessage(
-                message_id=message_id,
-                user_id=ctx.member.id,
-                username=ctx.member.username,
-                nickname=ctx.member.nickname
-                if ctx.member.nickname
-                else ctx.member.username,
-                message=message,
+    try:
+        if isinstance(ctx.member, hikari.InteractionMember) and isinstance(
+            channel := await ctx.fetch_channel(), hikari.TextableChannel
+        ):
+            await database.create_table()
+            message_id = await randomize_id(ctx.member.username)
+            await database.add_row(
+                AnonMessage(
+                    message_id=message_id,
+                    user_id=ctx.member.id,
+                    username=ctx.member.username,
+                    nickname=ctx.member.nickname
+                    if ctx.member.nickname
+                    else ctx.member.username,
+                    message=message,
+                )
             )
-        )
-        embeddings = config.embeddings["anonymous_message"]
-        await channel.send(
-            embed=hikari.Embed(
-                title=f"""{embeddings["title"]} • {message_id}""",
-                description=f"`{message}`",
-                color=embeddings["color"],
+            embeddings = config.embeddings["anonymous_message"]
+            await channel.send(
+                embed=hikari.Embed(
+                    title=f"""{embeddings["title"]} • {message_id}""",
+                    description=f"`{message}`",
+                    color=embeddings["color"],
+                )
             )
+<<<<<<< HEAD
         )
         await ctx.edit_initial_response("Message sent ✅")
         return
     await ctx.edit_initial_response("Something went wrong ❌")
 >>>>>>> a34eab6... feat: add functional slash command
+=======
+            await ctx.edit_initial_response("Message sent ✅")
+    except Exception as err:
+        await ctx.edit_initial_response(f"Something went wrong ❌\n`{err}`")
+>>>>>>> 0686459... docs: update docstrings
 
 
 @tanjun.as_loader
