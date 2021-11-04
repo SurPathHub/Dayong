bffe9440cf868c7b47e195aaa42d2a3ae7506b67
Hju Kneyck Flores <hjucode@gmail.com>
2021-11-05 02:17:20 +0800

    refactor: use aiohttp for external api requests

--- dayong/exts/apis.py
+++ dayong/exts/apis.py
@@ -4,28 +4,43 @@ dayong.exts.apis
 
 Module in charge of retrieving content from API endpoints.
 """
-import asyncio
 import json
 import urllib.request
-from typing import Any
+from typing import Any, Optional
 
-from dayong.abc import Client
+import aiohttp
+
 from dayong.exts.contents import ThirdPartyContent
 from dayong.settings import CONTENT_PROVIDER
 
 
-class RESTClient(Client):
+class RESTClient:
     """Represents a client for interacting with REST APIs."""
 
     _headers = {"User-Agent": "Mozilla/5.0"}
     _request = urllib.request.Request("http://127.0.0.1", headers=_headers)
+    _session: Optional[aiohttp.ClientSession] = None
 
-    @staticmethod
-    async def get_content(data: Any, *args: Any, **kwargs: Any) -> ThirdPartyContent:
-        loop = asyncio.get_running_loop()
-        resp = await loop.run_in_executor(None, urllib.request.urlopen, data)
-        data = await loop.run_in_executor(None, json.loads, resp.read())
-        return await ThirdPartyContent.parse(data, list(kwargs.values())[0])
+    async def create_session(self):
+        """Create client session."""
+        if not self._session:
+            self._session = aiohttp.ClientSession()
+
+    async def get_content(self, data: Any, *args: Any) -> "ThirdPartyContent":
+        """Parse and return fetched content.
+
+        Args:
+            data (Any): Response data from mail server.
+
+        Returns:
+            ThirdPartyContent: Representation of content from third-party
+                service/content provider.
+        """
+        await self.create_session()
+        assert isinstance(self._session, aiohttp.ClientSession)
+        resp = await self._session.get(data)
+        data = await resp.text()
+        return await ThirdPartyContent.parse(json.loads(data), args[0])
 
     async def get_devto_article(self, sort_by_date: bool = False) -> ThirdPartyContent:
         """Retrieve URLs of dev.to articles.
@@ -37,6 +52,7 @@ class RESTClient(Client):
         Returns:
             list[str]: List of article URLs.
         """
+
         request = self._request
 
         if sort_by_date:
@@ -44,4 +60,4 @@ class RESTClient(Client):
         else:
             request.full_url = f"""{CONTENT_PROVIDER["dev"]}/api/articles/"""
 
-        return await RESTClient.get_content(request, "canonical_url")
+        return await self.get_content(request.full_url, "canonical_url")
